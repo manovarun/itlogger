5c2fe4c2c77a1c6e036f069d631210220f31503b
manovarun <samvarun@gmail.comn>
2019-10-27 01:30:41 +0530

    Add Logs

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -1,4 +1,4 @@
-import { GET_LOGS, SET_LOADING, LOGS_ERROR } from './types';
+import { GET_LOGS, SET_LOADING, LOGS_ERROR, ADD_LOG } from './types';
 
 //Get logs from server
 export const getLogs = () => async dispatch => {
@@ -6,8 +6,34 @@ export const getLogs = () => async dispatch => {
     setLoading();
     const res = await fetch('/logs');
     const data = await res.json();
+    console.log(data);
     dispatch({
       type: GET_LOGS,
+      payload: data
+    });
+  } catch (err) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: err.response.data
+    });
+  }
+};
+
+//Add new log
+export const addLog = log => async dispatch => {
+  try {
+    setLoading();
+
+    const res = await fetch('/logs', {
+      method: 'POST',
+      body: JSON.stringify(log),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    const data = res.json();
+    dispatch({
+      type: ADD_LOG,
       payload: data
     });
   } catch (err) {
--- src/components/logs/AddLogModal.js
+++ src/components/logs/AddLogModal.js
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { addLog } from '../../actions/logActions';
 import M from 'materialize-css/dist/js/materialize';
 
-const AddLogModal = () => {
+const AddLogModal = ({ addLog }) => {
   const [message, setMessage] = useState('');
   const [attention, setAttention] = useState(false);
   const [tech, setTech] = useState('');
@@ -10,12 +13,20 @@ const AddLogModal = () => {
     if (message === '' || tech === '') {
       M.toast({ html: 'Please enter a message and tech' });
     } else {
-      console.log(message, attention, tech);
+      const newLog = {
+        message,
+        attention,
+        tech,
+        date: new Date()
+      };
+      console.log(newLog);
+      addLog(newLog);
+      M.toast({ html: `Log added by ${tech}` });
 
       //Clear Fields
       setMessage('');
-      setAttention(false);
       setTech('');
+      setAttention(false);
     }
   };
 
@@ -88,9 +99,16 @@ const AddLogModal = () => {
   );
 };
 
+AddLogModal.propTypes = {
+  addLog: PropTypes.func.isRequired
+};
+
 const modalStyle = {
   width: '75%',
   height: '75%'
 };
 
-export default AddLogModal;
+export default connect(
+  null,
+  { addLog }
+)(AddLogModal);
