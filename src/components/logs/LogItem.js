1dcfb5d3e4fd8a70c5c48b5adc977332979a55b8
manovarun <samvarun@gmail.comn>
2019-10-27 15:42:11 +0530

    Update Logs

--- src/components/logs/LogItem.js
+++ src/components/logs/LogItem.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { deleteLog } from '../../actions/logActions';
-import M from 'materialize-css/dist/js/materialize';
+import { deleteLog, setCurrent } from '../../actions/logActions';
+import M from 'materialize-css/dist/js/materialize.min.js';
 
 import Moment from 'react-moment';
 
-const LogItem = ({ log, deleteLog }) => {
+const LogItem = ({ log, deleteLog, setCurrent }) => {
   const onDelete = () => {
     deleteLog(log.id);
     M.toast({ html: 'Log Deleted' });
@@ -20,6 +20,7 @@ const LogItem = ({ log, deleteLog }) => {
           className={`modal-trigger ${
             log.attention ? 'red-text' : 'blue-text'
           }`}
+          onClick={() => setCurrent(log)}
         >
           {log.message}
         </a>
@@ -39,10 +40,11 @@ const LogItem = ({ log, deleteLog }) => {
 
 LogItem.propTypes = {
   log: PropTypes.object.isRequired,
-  deleteLog: PropTypes.func.isRequired
+  deleteLog: PropTypes.func.isRequired,
+  setCurrent: PropTypes.func.isRequired
 };
 
 export default connect(
   null,
-  { deleteLog }
+  { deleteLog, setCurrent }
 )(LogItem);
