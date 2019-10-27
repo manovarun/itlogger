eb382e60fed786ab8bd3375d7caced4d022e4cc4
manovarun <samvarun@gmail.comn>
2019-10-27 16:23:41 +0530

    Search Logs

--- src/reducers/logReducer.js
+++ src/reducers/logReducer.js
@@ -6,7 +6,8 @@ import {
   DELETE_LOG,
   SET_CURRENT,
   CLEAR_CURRENT,
-  UPDATE_LOG
+  UPDATE_LOG,
+  SEARCH_LOGS
 } from '../actions/types';
 
 const initialState = {
@@ -46,6 +47,11 @@ export default (state = initialState, action) => {
             return log;
           }
         })
+      };
+    case SEARCH_LOGS:
+      return {
+        ...state,
+        logs: action.payload
       };
     case SET_CURRENT:
       return {
